7a20e2ddddf3b44c5da13be06859ca7cc353d664
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-05-31 07:35:54 +0200

    Show help

--- reproduce.sh
+++ reproduce.sh
@@ -2,6 +2,6 @@
 
 wget --no-clobber https://github.com/davidemms/OrthoFinder/releases/download/2.5.5/OrthoFinder.tar.gz
 
-tar xzf OrthoFinder.tar.gz
+tar -xzf OrthoFinder.tar.gz
 
 ./OrthoFinder/orthofinder -h
