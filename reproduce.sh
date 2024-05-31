0cbf6c6ef687ab0a3072880a3c31e9aa36c2734c
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-05-31 07:37:05 +0200

    Run OrthoFinder

--- reproduce.sh
+++ reproduce.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+echo "Download OrthoFinder"
+
 wget --no-clobber https://github.com/davidemms/OrthoFinder/releases/download/2.5.5/OrthoFinder.tar.gz
+
+echo "Untar OrthoFinder"
 
 tar -xzf OrthoFinder.tar.gz
 
+echo "Show OrthoFinder's help"
+
 ./OrthoFinder/orthofinder -h
+
+echo "Run OrthoFinder"
+
+OrthoFinder/orthofinder -f OrthoFinder/ExampleData
